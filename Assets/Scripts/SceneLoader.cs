aae27086f214eadcf545a6ebb9f50381ed032e5f
CrmzonComet <lcameronbrew@gmail.com>
2022-04-03 20:13:20 -0400

    Start of Menu Functionality
    Created the scene loader script, setting up canvas for main title, created both Main Menu scene and Load Level scene.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    int lastLevelPlayed;
+
+    // play button
+    public void playGame() 
+    {
+    // change to the game scene
+        // we will base which level the player will continue on from the playerprefs
+        if(PlayerPrefs.GetInt("lastLevelPlayed") == 2)
+        {
+            SceneManager.LoadScene("LevelOne");
+        }
+        else if(PlayerPrefs.GetInt("lastLevelPlayed") == 4)
+        {
+            SceneManager.LoadScene("LevelTwo");
+        }
+        else if (PlayerPrefs.GetInt("lastLevelPlayed") == 5)
+        {
+            SceneManager.LoadScene("LevelThree");
+        }
+        else
+        {
+            SceneManager.LoadScene("LevelOne");
+        }
+
+        // Debug.Log("Launch lvl 1");
+    }
+
+    // load button
+    public void chooseLevel()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+    //quit button
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit!");
+    }
+
+    // level select screen
+    // level one button
+    public void levelOneSelect()
+    {
+        // change to level one game scene
+        SceneManager.LoadScene("LevelOne");
+        // Debug.Log("Launch lvl 1");
+    }
+    // level two button
+    public void levelTwoSelect()
+    {
+        // change to level two game scene
+        SceneManager.LoadScene("LevelTwo");
+        // Debug.Log("Launch lvl 2");
+    }
+
+    // level three button
+    public void levelThreeSelect()
+    {
+        // change to level three game scene
+        SceneManager.LoadScene("LevelThree");
+        // Debug.Log("Launch lvl 3");
+    }
+    
+}
